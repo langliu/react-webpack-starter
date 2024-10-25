7329db429189a71fb64afde89078d2d579e8090c
langliu <langliu1216@gmail.com>
2024-10-26 01:38:24 +0800

    feat(config): 添加样式解析

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import styles from './App.module.css'
+
 export default function App () {
+  console.log('App is running!', styles.AppContainer)
   return (
-    <div className="App">
+    <div>
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,10 @@ module.exports = {
   devServer: {
     hot: true,
     port: 3000,
-    open: true,
-    static: path.resolve(__dirname, 'public'),
+    open: false,
+    static: {
+      directory: path.resolve(__dirname, 'public'),
+    },
   },
   entry: './src/index.tsx',
   module: {
@@ -20,7 +22,37 @@ module.exports = {
         test: /\.tsx?$/,
         exclude: /node_modules/,
         use: 'ts-loader'
-      }
+      },
+      {
+        test: /\.module\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: {
+                auto: true,
+                localIdentName: '[local]__[hash:base64:10]',
+                exportLocalsConvention: 'asIs',
+              },
+              import: false,
+              // 默认情况下，css-loader 生成使用 ES 模块语法的 JS 模块。`import { container } from '...'`
+              // 如果你想使用 CommonJS 语法，你可以通过设置 esModule 选项来禁用 ES 模块语法。`import styles from './styles.module.css'`
+              esModule: false
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        exclude: /\.module\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
+        type: 'asset',
+      },
     ]
   },
   output: {
