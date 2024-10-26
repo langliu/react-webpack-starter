be8344819f56f8f605ff64f671ec0f80f6a502c2
langliu <langliu1216@gmail.com>
2024-10-26 22:40:49 +0800

    feat(config): biome进行代码格式化处理

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import styles from './App.module.css'
 
-export default function App () {
+export default function App() {
   console.log('App is running!', styles.AppContainer)
   return (
-    <div>
+    <div className={styles.AppContainer}>
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
